0c7f94fbba93d2587d96e2099be3e3c3a364da58
Rafaela Vitória Port <rafaela-vport@educar.rs.gov.br>
2023-09-03 18:28:30 -0300

    commitao

--- index.js
+++ index.js
@@ -3,49 +3,51 @@ const app = express();
 const port = 3000; 
 const expressLayouts = require('express-ejs-layouts');
 const session = require('express-session');
-const dotenv = require('dotenv').config();
+require('dotenv').config();
 const path = require('path');
 const usuarioController = require('./controllers/usuarioController');
 const personagemController = require('./controllers/personagemController');
 
 app.use(session({secret: 'chaveSecretaDemais'}));
 
-app.use(expressLayouts);
-app.set('layout', 'views/layouts/default/login');
-app.set('view engine', 'ejs');
-
-app.use(express.urlencoded({ extended: true })); 
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use((req, res, next) => {
-    if (req.session.usuario) {
-        console.log('Logado');
-        res.locals.layoutVariables ={
-            usuario: req.session.usuario
-        };
-        next();
-    }else{
-        console.log('Não logado');
-        if(req.url == '/cadastro' || req.url == '/perfil' || req.url == '/logout'){
-            res.redirect('/login?erro=2');
-        }else{
-            res.locals.layoutVariables ={
-                usuario: req.session.usuario
-            };
-            next();
-        }
-    }
+app.get('/login', (req, res) => {
+    app.set('layout', './layouts/default/login');
+    usuarioController.login(req, res);
 });
+
+//app.use(expressLayouts);
+//app.set('layout', 'views/layouts/default/login');
+//app.set('view engine', 'ejs');
+
+//app.use(express.urlencoded({ extended: true })); 
+//app.use(express.static(path.join(__dirname, 'public')));
+
+//app.use((req, res, next) => {
+    //if (req.session.usuario) {
+       // console.log('Logado');
+      //  res.locals.layoutVariables ={
+      //      usuario: req.session.usuario
+      //  };
+      //  next();
+    //}else{
+       // console.log('Não logado');
+       // if(req.url == '/cadastro' || req.url == '/perfil' || req.url == '/logout'){
+        //    res.redirect('/login?erro=2');
+      //  }else{
+       //     res.locals.layoutVariables ={
+       //         usuario: req.session.usuario
+       //     };
+       //     next();
+      //  }
+  //  }
+//});
 
 //app.get('/', (req, res) => {
     //app.set('layout', './layouts/login');
     //res.render('login');
 //});
 
-app.get('/login', (req, res) => {
-    app.set('layout', './layouts/default/login');
-    usuarioController.login(req, res);
-});
+
 
 //const express = require('express');
 //const bodyParser = require('body-parser');
