5e2c6f028b34b8520231eed170c28b577ebd022d
Rafaela Vitória Port <rafaela-vport@educar.rs.gov.br>
2023-09-03 17:10:38 -0300

    commit

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.use((req, res, next) => {
 });
 
 app.get('/', (req, res) => {
-    app.set('layout', './layouts/default/login');
+    app.set('layout', './layouts/login');
     res.render('login');
 });
 
