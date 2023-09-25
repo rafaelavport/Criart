f7fe68d19878af1bdae1cb31ba32d1743810c30e
Rafaela Vitória Port <rafaela-vport@educar.rs.gov.br>
2023-09-25 08:13:21 -0300

    title

--- public/js/login.js
+++ public/js/login.js
@@ -1,38 +1,7 @@
 let email = document.querySelector(email)
 let senha = document.querySelector(senha)
-//const crypto = require("crypto");
 
-//const DADOS_CRIPTOGRAFAR = {
-    //algoritmo : "aes256",
-    //segredo : "chaves",
-    //tipo : "hex"
-//};
+document.getElementById('fazerConta').onclick = function(){
+    window.location.href='/cadastro';
+  }
 
-//function criptografar(senha) {
-	//const cipher = crypto.createCipher(DADOS_CRIPTOGRAFAR.algoritmo, DADOS_CRIPTOGRAFAR.segredo);
-	//cipher.update(senha);
-	//return cipher.final(DADOS_CRIPTOGRAFAR.tipo);
-//};
-
-function Logar() {
-    //verifica se os campos estao preenchidos
-    //if(email && senha) {
-        //troca de janela ao clicar no botao
-    window.location.href = "file:///C:/Users/rafae/OneDrive/%C3%81rea%20de%20Trabalho/projeto/tela%20principal/index.html"
-    //} else{
-        //return alert ("preencha os campos!")
-    //}
-    //envia os dados para o servidor
-    //fetch('src/users/logar', {
-        //email, senha
-    //}).then(response => {
-    //    if(response.data.erro) {
-    //        return alert(response.data.erro)
-    //    } else {
-            //se tudo der certo, redireciona
-    //        window.location.href = '/privado'
-    //    }
-    //}).catch(erro => {
-    //    return alert(response.data.erro)
-    //})
-}
