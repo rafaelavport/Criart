dff3087e40c415fe9b469fda45d4b7d2d1d8a059
Rafaela Vitória Port <rafaela-vport@educar.rs.gov.br>
2023-09-25 07:59:51 -0300

    rota perso

--- public/js/home.js
+++ public/js/home.js
@@ -1,6 +1,7 @@
-function troca(){
-    window.location.href = "file:///C:/Users/rafae/OneDrive/%C3%81rea%20de%20Trabalho/projeto/tela-personagem-1/index.html"
+document.getElementById('botao').onclick = function(){
+  window.location.href='/personagem';
 }
+
 const box = document.querySelector('.box');
 
 window.addEventListener('scroll', () => {
--- public/js/personagem.js
+++ public/js/personagem.js
@@ -1,7 +1,5 @@
 
-function troca(){
-    window.location.href = "file:///C:/Users/rafae/OneDrive/%C3%81rea%20de%20Trabalho/projeto/tela-personagem-tabela/index.html"
-}
+
 
 function Mudarestado(el) {
         var display = document.getElementById(el).style.display;
