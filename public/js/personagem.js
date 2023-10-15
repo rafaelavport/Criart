a50ea9cba9f2c483e0fa19be6fc1ddea69055fbc
Rafaela Vitória Port <rafaela-vport@educar.rs.gov.br>
2023-10-15 20:56:26 -0300

    aaa

--- public/js/personagem.js
+++ public/js/personagem.js
@@ -1,11 +1,11 @@
 function Mudarestado(el) {
-    var elements = document.querySelectorAll('.personagem');
+    var elements = document.querySelectorAll('.area-selecao-personagem .quadrado');
     elements.forEach(function (element) {
         element.classList.add('hide');
-        element.classList.remove('show');
     });
-    var selectedElement = document.getElementById(el);
-    selectedElement.classList.remove('hide');
-    selectedElement.classList.add('show');
+    document.getElementById(el).classList.remove('hide');
+
+    var areaPersonagemCabeca = document.querySelector('.area-personagem-cabeça');
+    areaPersonagemCabeca.classList.remove('hide');
 }
 
