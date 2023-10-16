4c8134e8b37c75d66e7c0341cf07a53409cfeafa
Rafaela Vitória Port <rafaela-vport@educar.rs.gov.br>
2023-10-15 21:34:20 -0300

    js e ejs

--- public/js/personagem.js
+++ public/js/personagem.js
@@ -1,14 +1,18 @@
-function Mudarestado(el) {
-    var elements = document.querySelectorAll('.area-selecao-personagem .quadrado');
+function Mudarestado(el, imgSrc) {
+    var elements = document.querySelectorAll('.quadrado.personagem');
     elements.forEach(function (element) {
         element.classList.remove('selected');
     });
+
     var selectedElement = document.getElementById(el);
     selectedElement.classList.add('selected');
 
     var areaPersonagemCabeca = document.querySelector('.area-personagem-cabeça img');
-    var imageSource = selectedElement.querySelector('img').src;
-    areaPersonagemCabeca.src = imageSource;
+    areaPersonagemCabeca.src = imgSrc;
+}
+
+function troca() {
+    // Adicione a lógica para o que acontece quando você clica na seta direita (se necessário).
 }
 
 
