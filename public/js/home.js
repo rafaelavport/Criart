c029395231f2643a0801a6e114a99c906ac3b5d1
Rafaela Vitória Port <rafaela-vport@educar.rs.gov.br>
2023-10-30 08:39:56 -0300

    AAaa

--- public/js/home.js
+++ public/js/home.js
@@ -2,14 +2,26 @@ document.getElementById('botao').onclick = function(){
   window.location.href='/personagem';
 }
 
-const box = document.querySelector('.box1');
+const box1 = document.querySelector('.box1');
 
 window.addEventListener('scroll', () => {
   const scrollPosition = window.scrollY;
-  const boxPosition = box.offsetTop;
+  const boxPosition = box1.offsetTop;
   const windowHeight = window.innerHeight;
 
   if (scrollPosition > boxPosition - windowHeight + 400) {
-    box.style.opacity = 1;
+    box1.style.opacity = 1;
+  }
+});
+
+const box2 = document.querySelector('.box2');
+
+window.addEventListener('scroll', () => {
+  const scrollPosition = window.scrollY;
+  const boxPosition = box2.offsetTop;
+  const windowHeight = window.innerHeight;
+
+  if (scrollPosition > boxPosition - windowHeight + 400) {
+    box2.style.opacity = 1;
   }
 });
